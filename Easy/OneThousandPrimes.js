2bd1f7dc8c932e38a826a7424de2e880e0ff6758
Kevin <yangkevint@gmail.com>
2016-03-08 07:16:05 -0600

    Formatting

--- Easy/OneThousandPrimes.js
+++ Easy/OneThousandPrimes.js
@@ -1,17 +1,20 @@
 /**
  * Created by Kevin on 3/8/2016.
  */
-var sum = 0;
-var current_num = 2;
-var i= 1, j = 1, match = true;
-while(i<=1000) {
-    for(var k = 2; k < current_num; k++){
-        if(current_num%k == 0){
+
+var current_num = 2,
+    sum = 0,
+    i = 1,
+    match = true;
+
+while (i <= 1000) {
+    for (var k = 2; k < current_num; k++) {
+        if (current_num % k == 0) {
             match = false;
         }
     }
-    if(match == true){
-        sum = sum+current_num;
+    if (match == true) {
+        sum = sum + current_num;
         i++;
     }
     match = true;
